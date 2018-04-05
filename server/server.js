02b7a26524792dfef2250147b5dac1c3725334aa
Eric Julius <ejulius86@gmail.com>
2018-04-05 13:55:35 -0500

    minor formatting

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ const app = express();
 
 hbs.registerPartials(path.join(__dirname, '../views/partials'));
 app.set('view engine', 'hbs');
-app.use(fileUpload({limit: '50mb'}));
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(fileUpload({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
 app.use(express.static(publicPath));
 app.use('/', routes);
