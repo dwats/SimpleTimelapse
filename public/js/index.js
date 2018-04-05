1b0c8f7d1830a22c2230ac4921ce660b797b5446
Eric Julius <ejulius86@gmail.com>
2018-04-05 00:05:59 -0500

    Disabled client side video rendering.

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,43 @@
-const video = new Whammy.Video();
-const msPerFrame = 41.6;
+// const video = new Whammy.Video();
+// const msPerFrame = 41.66;
 
-fetch(`${url}/images`)
-  .then((res) => res.json())
-  .then((data) => {
-    const promises = data.frames.map(getCanvas);
-    Promise.all(promises)
-      .then((canvases) => {
-        console.log(canvases)
-        canvases.forEach(canvas => video.add(canvas, msPerFrame));
-        video.compile(false, (webm) => {
-          const url = window.URL.createObjectURL(webm);
-          const videoElm = document.getElementById('timelapse-video');
-          const progressElm = document.getElementById('progress');
-          const spinner = document.getElementById('timelapse-spinner');
-          spinner.style.display = 'none';
-          videoElm.src = url;
-          videoElm.style.display = '';   
-          progressElm.innerHTML = '';
-        });
-      });
+fetch(`${url}/timelapse`)
+  .then(() => {
+    const videoElm = document.getElementById('timelapse-video');
+    const spinner = document.getElementById('timelapse-spinner');
+    const progressElm = document.getElementById('progress');
+    spinner.style.display = 'none';
+    videoElm.src = '/vid/tmp.mp4';
+    videoElm.style.display = '';
+    progressElm.innerHTML = '';
   })
   .catch((e) => {
     console.log(e);
   });
+
+// fetch(`${url}/images`)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     const promises = data.frames.map(getCanvas);
+//     Promise.all(promises)
+//       .then((canvases) => {
+//         console.log(canvases)
+//         canvases.forEach(canvas => video.add(canvas, msPerFrame));
+//         video.compile(false, (webm) => {
+//           const url = window.URL.createObjectURL(webm);
+//           const videoElm = document.getElementById('timelapse-video');
+//           const progressElm = document.getElementById('progress');
+//           const spinner = document.getElementById('timelapse-spinner');
+//           spinner.style.display = 'none';
+//           videoElm.src = url;
+//           videoElm.style.display = '';
+//           progressElm.innerHTML = '';
+//         });
+//       });
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
 
 const getCanvas = (frame) => {
   return new Promise((resolve, reject) => {
@@ -38,8 +52,8 @@ const getCanvas = (frame) => {
       ctx.fillText(frame.timestamp, 25, 25);
       resolve(ctx);
     };
-    img.onerror = () => { 
-      console.log('error'); 
+    img.onerror = () => {
+      console.log('error');
       resolve();
     };
     img.src = `./images/${frame.filename}`;
