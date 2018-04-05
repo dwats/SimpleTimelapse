d6eaa9d8a72c4aa29788d9bd145d70e48358e21f
Eric Julius <ejulius86@gmail.com>
2018-04-05 13:56:37 -0500

    Moved timelapse gen to its own directory
    The timelapse gen is now separate from the server and runs on a timer.

---
+++ timelapseMicro/index.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const videoshow = require('videoshow');
+const { Image } = require('../server/models/image');
+
+const framesPerTimelapse = Number(process.env.FRAMES_PER_TIMELAPSE);
+const timeBetweenTimelapseGen = Number(process.env.TIME_BETWEEN_TIMELAPSE_GEN);
+const imageDir = path.join(__dirname, '../public/images');
+const videoDir = path.join(__dirname, '../public/media');
+const videoOptions = {
+  fps: 30,
+  loop: 1/30,
+  transition: false,
+  videoBitrate: 1024,
+  videoCodec: 'libx264',
+  size: '1024x768',
+  format: 'mp4',
+  pixelFormat: 'yuv420p'
+};
+
+function makeTimelapse(options, inputDir, outputDir) {
+  Image.findLast(framesPerTimelapse)
+    .then((frames) => {
+      if (!frames.length) return console.log(`[${new Date()}] timelapse has no frames!`);
+      const frameFilenames = frames.map((frame) => path.join(inputDir, frame.filename));
+
+      videoshow(frameFilenames, videoOptions)
+        .save(path.join(outputDir, 'timelapse.mp4'))
+        .on('error', (err, stdout, stderr) => {
+          console.log(`[${new Date()}] timelapse node error`, err);
+          console.log(`[${new Date()}] timelapse ffmpeg error `, stderr);
+        })
+        .on('end', (output) => {
+          console.log(`[${new Date()}] timelapse complete`, output);
+        });
+    })
+    .catch((e) => {
+      console.log(`[${new Date()}] timelapse promise error`, e);
+    });
+}
+
+setInterval(
+  () => makeTimelapse(videoOptions, imageDir, videoDir),
+  timeBetweenTimelapseGen
+);
